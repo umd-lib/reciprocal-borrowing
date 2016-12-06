12cf8eea6e816cf0505ddd6a2ff10a08cfd403ce
Peter Eichman <peichman@umd.edu>
2016-12-06 10:52:55 -0500

    LIBITD-633. Added environment banner as a helper.
    It determines the environment (Local, Development, or Staging) from examining the RAILS_ENV and the server's hostname.
    
    https://issues.umd.edu/browse/LIBITD-633

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def umd_lib_environment_banner
+    if Rails.env.development?
+      environment = 'Local'
+    else
+      hostname = `hostname -s`
+      if hostname =~ /dev$/
+        environment = 'Development'
+      elsif hostname =~ /stage$/
+        environment = 'Staging'
+      end
+    end
+    if environment
+      content_tag :div, "#{environment} Environment",
+        class: 'environment-banner',
+        id: "environment-#{environment.downcase}"
+    end
+  end
 end
