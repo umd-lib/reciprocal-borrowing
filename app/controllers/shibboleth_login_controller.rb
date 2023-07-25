05581906bfbd14360a32211dcd0194591987abae
David P. Steelman <dsteelma@umd.edu>
2023-07-25 11:08:35 -0400

    LIBITD-2409. Fixed "Accessing hashes ... by non-symbol keys" warnings
    Fixed "Accessing hashes returned from config_for by non-symbol keys is
    deprecated" deprecation warnings
    
    https://umd-dit.atlassian.net/browse/LIBITD-2409

--- app/controllers/shibboleth_login_controller.rb
+++ app/controllers/shibboleth_login_controller.rb
@@ -1,17 +1,17 @@
 class ShibbolethLoginController < ApplicationController
   def home
     organizations = Rails.configuration.shibboleth_config['organizations']
-    @org_list = organizations.values.sort_by { |v| v['display_order'] }
+    @org_list = organizations.values.sort_by { |v| v[:display_order] }
   end
 
   def authenticate # rubocop:disable Metrics/AbcSize
     @lending_org_code = params['lending_org_code']
     organizations = Rails.configuration.shibboleth_config['organizations']
 
-    if organizations.key?(@lending_org_code)
-      @org_list = organizations.values.sort_by { |v| v['display_order'] }
-      lending_org = organizations[@lending_org_code]
-      @lending_org_name = lending_org['name']
+    if organizations.key?(@lending_org_code.to_sym)
+      @org_list = organizations.values.sort_by { |v| v[:display_order] }
+      lending_org = organizations[@lending_org_code.to_sym]
+      @lending_org_name = lending_org[:name]
       session[:lending_org_code] = @lending_org_code
     else
       # Was not given a valid org_code, treat as 404 error
@@ -25,9 +25,9 @@ class ShibbolethLoginController < ApplicationController
     sp_login_url = ENV['SP_LOGIN_URL'] || '/Shibboleth.sso/Login'
     callback_url = ENV['CALLBACK_URL'] || '/attributes'
 
-    if organizations.key?(org_code)
-      org = organizations[org_code]
-      idp_entity_id = org['idp_entity_id']
+    if organizations.key?(org_code.to_sym)
+      org = organizations[org_code.to_sym]
+      idp_entity_id = org[:idp_entity_id]
       session[:auth_org_code] = org_code
       url_encoded_idp_entity_id = ERB::Util.url_encode(idp_entity_id)
       url_with_callback = "#{sp_login_url}?entityID=#{url_encoded_idp_entity_id}&target=#{callback_url}"
--- test/controllers/shibboleth_login_controller_test.rb
+++ test/controllers/shibboleth_login_controller_test.rb
@@ -9,7 +9,7 @@ class ShibbolethLoginControllerTest < ActionController::TestCase # rubocop:disab
   test 'home should contain an entry for each organization' do
     get :home
     organizations = Rails.configuration.shibboleth_config['organizations']
-    org_list = organizations.values.sort_by { |v| v['display_order'] }
+    org_list = organizations.values.sort_by { |v| v[:display_order] }
 
     # Number of entries should be the same as number of organizations
     org_count = org_list.size
@@ -18,8 +18,8 @@ class ShibbolethLoginControllerTest < ActionController::TestCase # rubocop:disab
     end
 
     org_list.each do |org|
-      org_code = org['code']
-      org_name = org['name']
+      org_code = org[:code]
+      org_name = org[:name]
 
       entry_id = "#{org_code}_entry"
       assert_select "ul#organization_list>li##{entry_id}" do
@@ -30,27 +30,27 @@ class ShibbolethLoginControllerTest < ActionController::TestCase # rubocop:disab
 
   test 'authenticate should exist for each organization' do
     organizations = Rails.configuration.shibboleth_config['organizations']
-    org_list = organizations.values.sort_by { |v| v['display_order'] }
+    org_list = organizations.values.sort_by { |v| v[:display_order] }
     org_list.each do |lending_org|
-      lending_org_code = lending_org['code']
+      lending_org_code = lending_org[:code]
       get :authenticate, params: { lending_org_code: lending_org_code }
       assert_response :success
 
       # authenicate page should contain organization full name
-      assert_select '.lending-org-name', text: lending_org['name']
+      assert_select '.lending-org-name', text: lending_org[:name]
     end
   end
 
   test 'authenticate should not contain link to lending org in org list' do
     organizations = Rails.configuration.shibboleth_config['organizations']
-    org_list = organizations.values.sort_by { |v| v['display_order'] }
+    org_list = organizations.values.sort_by { |v| v[:display_order] }
     org_list.each do |lending_org|
-      lending_org_code = lending_org['code']
+      lending_org_code = lending_org[:code]
       get :authenticate, params: { lending_org_code: lending_org_code }
       assert_response :success
 
       org_list.each do |initiate_org|
-        initiate_org_code = initiate_org['code']
+        initiate_org_code = initiate_org[:code]
         list_id = "#{initiate_org_code}_entry"
 
         # Make sure each organization has an initiate link, except for the
