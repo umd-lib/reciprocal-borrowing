fc865d5ecfef4c9029e8547da0ddebc27f6a387d
David P. Steelman <dsteelma@umd.edu>
2023-07-25 08:24:57 -0400

    LIBITD-2409. Updates based on v5.2.8.1 sample project
    Compared files to a "new" sample reciprocal-borrowing project
    generated using Rails v5.1.7 and the following command:
    
    ```
    $ rails new reciprocal-borrowing --skip-active-record --skip-action-cable --skip-action-mailer --skip-active-storage --skip-bootsnap --skip-system-test
    ```
    
    ActiveRecord, ActionCable, ActionMailer, ActiveStorage, bootsnap, and
    system tests were skipped because they are not neeeded by
    reciprocal-borrowing.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2409

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = false
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -38,7 +37,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -74,6 +72,6 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 end
