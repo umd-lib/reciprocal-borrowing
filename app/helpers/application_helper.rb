f1ececc97264b03a098a08a6877bdb25257f418e
David P. Steelman <dsteelma@umd.edu>
2023-07-26 11:07:30 -0400

    LIBITD-2421. Updated environment banner to use environment variables
    Largely using the code from the "umd-lib/umd_lib_style" repository,
    added a "UmdLibEnvironmentBannerHelper" module that displays the
    SSDR environment banner using a configuration defined in
    environment variables.
    
    Note: The helper was renamed from "UMD"LibEnvironmentBannerHelper to
    "Umd"LibEnvironmentBannerHelper, because that was what Rails 6.1
    was expecting as the helper name.
    
    Removed the existing environment banner code from
    "application_helper.rb" because it was no longer needed.
    
    Removed from "umd_lib_environment_banner.scss" CSS settings for
    the "development" and "staging" environments that are no longer needed.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2421

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,2 @@
 module ApplicationHelper
-  def umd_lib_environment_banner
-    return unless environment
-
-    content_tag :div, "#{environment} Environment",
-                class: 'environment-banner',
-                id: "environment-#{environment.downcase}"
-  end
-
-  private
-
-    # Returns a string indicating the current local/dev/stage environment,
-    # or nil for the production environment.
-    def environment
-      return 'Local' if Rails.env.development?
-
-      hostname = `hostname -s`
-      return 'Development' if hostname =~ /dev$/
-      return 'Staging' if hostname =~ /stage$/
-
-      # Otherwise returns nil, indicating production environment
-    end
 end
